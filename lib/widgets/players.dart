9ef996994977fd82b40208a6703a66a65672f746
Hleb Bandarenka <hleb.bandarenka@intelex.com>
2024-11-11 20:32:42 -0800

    added NumberOfPlayers provider

--- lib/widgets/players.dart
+++ lib/widgets/players.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:root_randomizer/repository/providers/number_of_players_provider.dart';
 
 class PlayersWidget extends ConsumerWidget {
   const PlayersWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final numberOfPlayers = ref.watch(numberOfPlayersProvider);
+    final notifier = ref.read(numberOfPlayersProvider.notifier);
     return Expanded(
         child: Row(
       children: [
-        Text("Players: "),
-        ElevatedButton(onPressed: () {}, child: Text("-1")),
-        Text("5"),
-        ElevatedButton(onPressed: () {}, child: Text("+1")),
+        const Text("Number of players: "),
+        ElevatedButton(onPressed: notifier.decrement, child: const Text("-1")),
+        Text(numberOfPlayers.toString()),
+        ElevatedButton(onPressed: notifier.increment, child: const Text("+1")),
       ],
     ));
   }
