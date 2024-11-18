7537b545397b748128cbe0e063b35cd3bd555400
Hleb Bandarenka <hleb.bandarenka@intelex.com>
2024-11-17 19:31:07 -0800

    bumped version

--- lib/repository/providers/randomization_provider.dart
+++ lib/repository/providers/randomization_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:root_randomizer/repository/providers/balance_provider.dart';
 import 'package:root_randomizer/repository/providers/factions_filter_provider.dart';
