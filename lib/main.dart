258c08539adaa37ca2df36bb734cd11de910a994
Hleb Bandarenka <hleb.bandarenka@intelex.com>
2024-11-19 18:44:13 -0800

    Put fonts into the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:root_randomizer/screens/main.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const ProviderScope(child: MainApp()));
 }
 
